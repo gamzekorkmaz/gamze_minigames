3077bc17cb465b6d100aae3e2ba49080f4b77479
gamzekorkmazerdem <gamze.korkmaz.2709@gmail.com>
2024-08-09 16:46:19 +0300

    Added pagination and popup modules

---
+++ Assets/Scripts/Minigames/UI/Pages/GameListPage.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using Core.UI.Pagination;
+using UnityEngine;
+
+namespace Minigames.UI.Pages
+{
+    public class GameListPage : Page
+    {
+        protected override void OnClosed()
+        {
+            
+        }
+
+        protected override void OnOpened()
+        {
+            
+        }
+    }
+}
+
+
